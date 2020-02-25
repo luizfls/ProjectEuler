3d6a305cc3a5f2bd29e8954b52c33a65f47457e9
Luiz Felipe Silva <luiz.felipe7@gmail.com>
2020-02-25 02:57:07 -0300

    Solution for problem #99
    The numbers are scaled logarithmically, thus avoiding the large numbers from the exponentiation but keeping their ordering.

---
+++ 99.cpp
@@ -0,0 +1,32 @@
+#include <algorithm>
+#include <cmath>
+#include <fstream>
+#include <iostream>
+#include <unordered_map>
+
+using Line2Result = std::unordered_map<unsigned int, double>;
+
+Line2Result loadFile()
+{
+  Line2Result lines;
+  std::ifstream file("base_exp.txt");
+  for(unsigned int i = 1; i <= 1000; i++)
+  {
+    int base, exponent;
+    char comma;
+    file >> base >> comma >> exponent;
+    double result = exponent * std::log10(base);
+    lines[i] = result;
+  }
+  return lines;
+}
+
+int main(int argc, char* argv[])
+{
+    auto lines = loadFile();
+    auto cmp = [](const Line2Result::value_type& a, const Line2Result::value_type& b)
+               { return a.second < b.second; };
+    auto maxIt = std::max_element(lines.begin(), lines.end(), cmp);
+    std::cout << maxIt->first << std::endl;
+    return 0;
+}
